1df3c16d74e389614905d87dc2becf556c62c941
Tara <tara.ganepola@faculty.ai>
2020-12-14 15:40:32 +0000

    minor changes to setup.py

--- setup.py
+++ setup.py
@@ -5,9 +5,8 @@ setuptools.setup(
     version="0.1.0",
     description="Cleaning pipeline for the the National"
         + "COVID-19 Chest Imaging Database (NCCID) clinical data.",
-    author="Faculty",
-    author_email="info@faculty.ai",
-    url="",
+    author="NHSX",,
+    url="git@github.com:nhsx/nccid-cleaning.git",
     packages=setuptools.find_packages(),
     install_requires=["pydicom", "pandas", "tqdm"],
     extras_require={
