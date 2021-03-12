31d1a1130fede685db1b20a8c57726758719910c
Tara <tara.ganepola@faculty.ai>
2021-03-12 17:52:48 +0000

    added setuptools_scm versioning

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@ import setuptools
 
 setuptools.setup(
     name="nccid_cleaning",
-    version="0.1.0",
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
     description="Cleaning pipeline for the the National"
         + "COVID-19 Chest Imaging Database (NCCID) clinical data.",
     author="NHSX",
